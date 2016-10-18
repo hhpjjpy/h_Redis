e3d9b36823c96b3f6ebc04208bae2acbb34d796d
hzhuhaiping <hzhuhaiping@corp.netease.com>
2016-10-18 11:11:16 +0800

    hash表接口头文件

---
+++ base_struct/hdict.h
@@ -0,0 +1,46 @@
+﻿#ifndef HDICT_H
+#define HDICT_H
+
+typedef struct dictEntry{
+	void *key;
+	void *val;
+	struct dictEntry *next;
+}dictEntry;//链地址法解决冲突
+
+typedef struct dictType{
+	unsigned int(*hashFunction)(const void *key);
+	void* (*keyDup)(void *privdata,const void  *key);
+	void* (*valDup)(void *privdata, const void *obj);
+	int (*keyCompare)(void *key1, void *key2);
+	void (*keyFree)(void *key);
+	void (*valFree)(void *val);
+}dictType;
+
+typedef struct dictht{
+	dictEntry **table;
+	unsigned long size;
+	unsigned long used;
+}dictht;
+
+typedef struct dict{
+	dictType *type;
+	dictht ht[2];
+	long rehashidx;
+}dict;
+
+#define dictHashKey(d,key) (d)->type->hashFunction(key)
+
+
+dict *dictCreate(dictType *type);
+int dictExpand(dict *d, unsigned long size);
+int dictAdd(dict *d,void *key,void *val);
+int dictReplace(dict *d,void *key,void *val);
+void dictDelete(dict *d,const void *key);
+dictEntry* dictFind(dict *d,const void *key);
+void* dictFetchValue(dict *d,const void *key);
+int dictResize(dict *d);
+int dictRehash(dict *d,int n);
+int dictRehashMilliseconeds(dict *d,int ms);
+void dictEmpty(dict *d);
+
+#endif
